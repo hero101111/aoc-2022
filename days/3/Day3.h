e49b1be280a0c1d92d8c286c3060317167981820
Gabriel Diaconita <diaconitagabriel@gmail.com>
2022-12-03 07:21:06 +0200

    Add solution for day 3

--- days/3/Day3.h
+++ days/3/Day3.h
@@ -27,13 +27,67 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 31;
+    LL ret = 0;
+    for (auto d : mData)
+    {
+      set<char> left, right;
+      size_t n = d.size();
+      for (int i = 0; i < n; ++i)
+      {
+        if (i < n / 2)
+          left.insert(d[i]);
+        else
+          right.insert(d[i]);
+      }
+      
+      set<char> inter;
+      for (auto c : left)
+        if (right.find(c) != end(right))
+          inter.insert(c);
+      
+      LL val;
+      for (auto c : inter)
+      {
+        if (c >= 'A' and c <= 'Z')
+          val = c - 'A' + 27;
+        else
+          val = c - 'a'+ 1;
+      }
+      ret += val;
+    }
+    
     return ret;
   }
 
   LL DoWork2()
   {
-    LL ret = 32;
+    LL ret = 0;
+    for (int i = 0; i < mData.size() - 1; i += 3)
+    {
+      set<char> s1, s2, s3;
+      for (auto c : mData[i])
+        s1.insert(c);
+      for (auto c : mData[i + 1])
+        s2.insert(c);
+      for (auto c : mData[i + 2])
+        s3.insert(c);
+      
+      set<char> inter;
+      for (auto c : s1)
+        if (s2.find(c) != end(s2) && s3.find(c) != end(s3))
+          inter.insert(c);
+      
+      LL val = 0;
+      for (auto c : inter)
+      {
+        if (c >= 'A' and c <= 'Z')
+          val = c - 'A' + 27;
+        else
+          val = c - 'a'+ 1;
+      }
+      ret += val;
+    }
+    
     return ret;
   }
   
@@ -54,8 +108,8 @@ public:
   bool Test() override
   {
     mCurrentInput = "test";
-    assert(Part1() != "");
-    assert(Part2() != "");
+    //assert(Part1() != "");
+    //assert(Part2() != "");
     return true;
   }
 };
