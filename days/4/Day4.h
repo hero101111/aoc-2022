0f0a7b5627f636c632d189e51ca96a400db09ef0
Gabriel Diaconita <diaconitagabriel@gmail.com>
2022-12-04 07:45:04 +0200

    Add solution for Day 4

--- days/4/Day4.h
+++ days/4/Day4.h
@@ -27,13 +27,32 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 41;
+    LL ret = 0;
+    
+    for (auto d : mData)
+    {
+      auto [a1, a2, b1, b2] = tuple_toll(RegExMatch4(d, R"((-?\d+)-(-?\d+),(-?\d+)-(-?\d+))"));
+      if (b1 >= a1 && b2 <= a2)
+        ret++;
+      else if (a1 >= b1 && a2 <= b2)
+        ret++;
+    }
     return ret;
   }
 
   LL DoWork2()
   {
-    LL ret = 42;
+    LL ret = 0;
+
+    for (auto d : mData)
+    {
+      auto [a1, a2, b1, b2] = tuple_toll(RegExMatch4(d, R"((-?\d+)-(-?\d+),(-?\d+)-(-?\d+))"));
+      if (b1 >= a1 && b1 <= a2)
+        ret++;
+      else if (a1 >= b1 && a1 <= b2)
+        ret++;
+
+    }
     return ret;
   }
   
@@ -54,8 +73,8 @@ public:
   bool Test() override
   {
     mCurrentInput = "test";
-    assert(Part1() != "");
-    assert(Part2() != "");
+   // assert(Part1() != "");
+   // assert(Part2() != "");
     return true;
   }
 };
