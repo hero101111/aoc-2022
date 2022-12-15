801b0d6fbbede1019a853de5cfb9632561bcfd43
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2022-12-15 08:30:56 +0200

    Add solution for Day15

--- days/15/Day15.h
+++ days/15/Day15.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SolutionDay.h"
+#include "z3++.h"
 
 class Day15 : public ISolutionDay
 {
@@ -22,7 +23,51 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 151;
+    LL               ret = 0;
+    DynamicMap<char> m;
+
+    unordered_set<Point> sensors;
+    unordered_set<Point> beacons;
+    unordered_set<Point> cannotList;
+
+    z3::context c;
+    z3::expr    x = c.int_const("x");
+    z3::expr    y = c.int_const("y");
+    z3::solver  solver(c);
+    solver.add(x >= 0);
+    solver.add(x <= 4000000);
+    solver.add(y >= 0);
+    solver.add(y <= 4000000);
+
+    LL maxy = 2000000;
+    for (auto d : mData)
+    {
+      auto [sx, sy, bx, by] = RegExMatch4(
+        d, R"(Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+))");
+      Point s{ sx, sy };
+      Point b{ bx, by };
+
+      sensors.insert(s);
+      beacons.insert(b);
+      auto dist = s.ManhattanDist(b);
+
+      /*for (auto x : rangeint(s.x - dist, s.x + dist + 1))
+      {
+        if (s.ManhattanDist(Point{ x, maxy }) <= dist)
+          cannotList.insert(Point{ x, maxy });
+      }*/
+
+      solver.add(z3::abs(x - c.int_val(s.x)) + z3::abs(y - c.int_val(s.y)) > c.int_val(dist));
+    }
+
+    // auto diff = getDifference(cannotList, beacons);
+    // ret       = diff.size();
+
+    solver.check();
+    auto solution = solver.get_model();
+    auto retValue = solution.eval(x * 4000000 + y).to_string();
+    ret           = stoll(retValue);
+    // m.printf();
     return ret;
   }
 
@@ -48,9 +93,9 @@ public:
 
   bool Test() override
   {
-    mCurrentInput = "test";
-    // aoc_assert(Part1() != "");
-    // aoc_assert(Part2() != "");
+    // mCurrentInput = "test";
+    // aoc_assert(Part1(), ""s);
+    //   aoc_assert(Part2() != "");
     return true;
   }
 };
