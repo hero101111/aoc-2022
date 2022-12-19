f1dbb8a03658c8bf407f487ea184355c3703ee37
Gabriel Diaconita <diaconitagabriel@gmail.com>
2022-12-19 07:30:44 +0200

    Day19 drat

--- days/19/Day19.h
+++ days/19/Day19.h
@@ -22,7 +22,68 @@ public:
 
   LL DoWork1()
   {
-    LL ret = 191;
+    LL ret = 0;
+    
+    for (auto d : mData)
+    {
+      string rgx = R"(Blueprint (.+): Each ore robot costs (.+) ore. Each clay robot costs (.+) ore. Each obsidian robot costs (.+) ore and (.+) clay. Each geode robot costs (.+) ore and (.+) obsidian.)";
+      auto [blueprint, oreRCost, clayRCost, obsRCostOre, obsRCostClay, geodeRCostOre, geodeRCostObidian] = RegExMatch7(d, rgx);
+      
+      map<string, map<string, LL>> costs;
+      costs["ore"]["ore"] = stoll(oreRCost);
+      costs["clay"]["ore"] = stoll(clayRCost);
+      costs["obs"]["ore"] = stoll(obsRCostOre);
+      costs["obs"]["clay"] = stoll(obsRCostClay);
+      costs["geode"]["ore"] = stoll(geodeRCostOre);
+      costs["geode"]["obs"] = stoll(geodeRCostObidian);
+      
+      LL minute = 1;
+      unordered_map<string, LL> res;
+      unordered_map<string, LL> robots;
+      robots["ore"] = 1;
+      
+      auto construct = [&](string type)
+      {
+        auto cost = costs[type];
+        
+        bool canAfford = true;
+        while (true)
+        {
+          for (auto [costItemName, costItemVal] : cost)
+          {
+            if (res[costItemName] < costItemVal)
+              canAfford = false;
+          }
+          if (!canAfford)
+            break;
+          
+          for (auto [costItemName, costItemVal] : cost)
+          {
+            res[costItemName] -= costItemVal;
+          }
+          robots[type] += 1;
+        }
+      };
+      
+      while (true)
+      {
+        // collect
+        for (auto r : robots)
+          res[r.first] += r.second;
+        
+        // construct geode robot
+        construct("geode");
+        construct("obs");
+        construct("clay");
+        construct("ore");
+        
+        
+        if (minute++ == 24) break;
+      }
+      LL qualityLevel = res["geode"] * stoll(blueprint);
+      ret += qualityLevel;
+    }
+    
     return ret;
   }
 
@@ -49,7 +110,7 @@ public:
   bool Test() override
   {
     mCurrentInput = "test";
-    // aoc_assert(Part1() != "");
+     aoc_assert(Part1(), ""s);
     // aoc_assert(Part2() != "");
     return true;
   }
