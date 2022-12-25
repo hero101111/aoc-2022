69b045e57d5388e65897135e1e266fe78471c252
Gabriel Diaconita <gabriel.diaconita@caphyon.com>
2022-12-25 08:04:36 +0200

    Add solution for day 25

--- days/25/Day25.h
+++ days/25/Day25.h
@@ -20,15 +20,59 @@ public:
 
   string GetDay() override { return "25"; }
 
-  LL DoWork1()
+  LL toBaseDec(string snafuN)
   {
-    LL ret = 251;
+    LL ret = 0;
+
+    LL pw = 0;
+    for (auto it = snafuN.rbegin(); it != snafuN.rend(); it++)
+    {
+      LL pw5 = pow(5, pw);
+      LL digit;
+      if (isdigit(*it))
+        digit = stoll(""s + *it);
+      else if (*it == '=')
+        digit = -2;
+      else if (*it == '-')
+        digit = -1;
+      ret += pw5 * digit;
+      pw++;
+    }
     return ret;
   }
 
-  LL DoWork2()
+  string toBaseSNAFU(LL decN)
   {
-    LL ret = 252;
+    string ret;
+
+    if (decN == 0)
+      return "";
+    else
+      switch (decN % 5)
+      {
+      case 0:
+        return toBaseSNAFU(decN / 5) + "0";
+      case 1:
+        return toBaseSNAFU(decN / 5) + "1";
+      case 2:
+        return toBaseSNAFU(decN / 5) + "2";
+      case 3:
+        return toBaseSNAFU((decN + 2) / 5) + "=";
+      case 4:
+        return toBaseSNAFU((decN + 2) / 5) + "-";
+      }
+    return ret;
+  }
+
+  string DoWork()
+  {
+    LL sum = 0;
+    for (auto d : mData)
+    {
+      sum += toBaseDec(d);
+    }
+
+    string ret = toBaseSNAFU(sum);
     return ret;
   }
 
@@ -36,21 +80,21 @@ public:
   {
     ReadData();
 
-    return std::to_string(DoWork1());
+    return DoWork();
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork2());
+    return "MERRY CHRISTMAS"s;
   }
 
   bool Test() override
   {
-    mCurrentInput = "test";
-    // aoc_assert(Part1() != "");
-    // aoc_assert(Part2() != "");
+    mCurrentInput = "input";
+    aoc_assert(Part1(), "2-121-=10=200==2==21"s);
+    aoc_assert(Part2(), "MERRY CHRISTMAS"s);
     return true;
   }
 };
